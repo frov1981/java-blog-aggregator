8ed5cd97471a7d569bea3997323d7a2121dd28b1
Fernando Ortega <frov1981@gmail.com>
2017-11-12 14:09:21 -0500

    Configuration postgresql

--- src/main/java/cz/jiripinkas/jba/service/InitDbService.java
+++ src/main/java/cz/jiripinkas/jba/service/InitDbService.java
@@ -38,79 +38,82 @@ public class InitDbService {
 
 	@PostConstruct
 	public void init() {
-		Role roleUser = new Role();
-		roleUser.setName("ROLE_USER");
-		roleRepository.save(roleUser);
 
-		Role roleAdmin = new Role();
-		roleAdmin.setName("ROLE_ADMIN");
-		roleRepository.save(roleAdmin);
+		if (roleRepository.findByName("ROLE_ADMIN") == null) {
+			Role roleUser = new Role();
+			roleUser.setName("ROLE_USER");
+			roleRepository.save(roleUser);
 
-		User userAdmin = new User();
-		userAdmin.setName("admin");
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		userAdmin.setPassword(encoder.encode("admin"));
-		userAdmin.setEnabled(true);
-		List<Role> roles = new ArrayList<Role>();
-		roles.add(roleAdmin);
-		roles.add(roleUser);
-		userAdmin.setRoles(roles);
-		userRepository.save(userAdmin);
+			Role roleAdmin = new Role();
+			roleAdmin.setName("ROLE_ADMIN");
+			roleRepository.save(roleAdmin);
 
-		Blog blogJava = new Blog();
-		blogJava.setName("JavaVids");
-		blogJava.setUrl("http://feeds.feedburner.com/javavids?format=xml");
-		blogJava.setUser(userAdmin);
-		blogRepository.save(blogJava);
+			User userAdmin = new User();
+			userAdmin.setName("admin");
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			userAdmin.setPassword(encoder.encode("admin"));
+			userAdmin.setEnabled(true);
+			List<Role> roles = new ArrayList<Role>();
+			roles.add(roleAdmin);
+			roles.add(roleUser);
+			userAdmin.setRoles(roles);
+			userRepository.save(userAdmin);
 
-		Item item1 = new Item();
-		item1.setBlog(blogJava);
-		item1.setTitle("First");
-		item1.setLink("http://www.javavids.com");
-		item1.setPublishedDate(new Date());
-		itemRepository.save(item1);
+			Blog blogJava = new Blog();
+			blogJava.setName("JavaVids");
+			blogJava.setUrl("http://feeds.feedburner.com/javavids?format=xml");
+			blogJava.setUser(userAdmin);
+			blogRepository.save(blogJava);
 
-		Item item2 = new Item();
-		item2.setBlog(blogJava);
-		item2.setTitle("Second");
-		item2.setLink("http://www.javavidsOkis.com");
-		item2.setPublishedDate(new Date());
-		itemRepository.save(item2);
+			Item item1 = new Item();
+			item1.setBlog(blogJava);
+			item1.setTitle("First");
+			item1.setLink("http://www.javavids.com");
+			item1.setPublishedDate(new Date());
+			itemRepository.save(item1);
 
-		Item item3 = new Item();
-		item3.setBlog(blogJava);
-		item3.setTitle("Thrid");
-		item3.setLink("http://www.oleole.com");
-		item3.setPublishedDate(new Date());
-		itemRepository.save(item3);
+			Item item2 = new Item();
+			item2.setBlog(blogJava);
+			item2.setTitle("Second");
+			item2.setLink("http://www.javavidsOkis.com");
+			item2.setPublishedDate(new Date());
+			itemRepository.save(item2);
 
-		Item item4 = new Item();
-		item2.setBlog(blogJava);
-		item2.setTitle("Four");
-		item2.setLink("http://www.xvideos.com");
-		item2.setPublishedDate(new Date());
-		itemRepository.save(item4);
+			Item item3 = new Item();
+			item3.setBlog(blogJava);
+			item3.setTitle("Thrid");
+			item3.setLink("http://www.oleole.com");
+			item3.setPublishedDate(new Date());
+			itemRepository.save(item3);
 
-		Item item5 = new Item();
-		item5.setBlog(blogJava);
-		item5.setTitle("Five");
-		item5.setLink("http://www.kids.com");
-		item5.setPublishedDate(new Date());
-		itemRepository.save(item5);
+			Item item4 = new Item();
+			item2.setBlog(blogJava);
+			item2.setTitle("Four");
+			item2.setLink("http://www.xvideos.com");
+			item2.setPublishedDate(new Date());
+			itemRepository.save(item4);
 
-		Item item6 = new Item();
-		item6.setBlog(blogJava);
-		item6.setTitle("Six");
-		item6.setLink("http://www.underboard.com");
-		item6.setPublishedDate(new Date());
-		itemRepository.save(item6);
+			Item item5 = new Item();
+			item5.setBlog(blogJava);
+			item5.setTitle("Five");
+			item5.setLink("http://www.kids.com");
+			item5.setPublishedDate(new Date());
+			itemRepository.save(item5);
 
-		Item item7 = new Item();
-		item7.setBlog(blogJava);
-		item7.setTitle("Seven");
-		item7.setLink("http://www.vids.com");
-		item7.setPublishedDate(new Date());
-		itemRepository.save(item7);
+			Item item6 = new Item();
+			item6.setBlog(blogJava);
+			item6.setTitle("Six");
+			item6.setLink("http://www.underboard.com");
+			item6.setPublishedDate(new Date());
+			itemRepository.save(item6);
+
+			Item item7 = new Item();
+			item7.setBlog(blogJava);
+			item7.setTitle("Seven");
+			item7.setLink("http://www.vids.com");
+			item7.setPublishedDate(new Date());
+			itemRepository.save(item7);
+		}
 
 	}
 
